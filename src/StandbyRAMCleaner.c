5c6f08e795c75b8fe703104cfe7d1616f2643a8f
theKorzh <diman@korzh.pro>
2025-11-03 06:50:18 +0300

    Initial

--- src/StandbyRAMCleaner.c
+++ src/StandbyRAMCleaner.c
@@ -1,29 +1,42 @@
-/*
- * Standby RAM Cleaner Service
- * ---------------------------
- * Monitors free memory and purges the Standby List on Windows.
- *
- * Copyright (C) 2025 theKorzh
- *
- * This file is part of the Standby RAM Cleaner Service project.
- *
- * The program is licensed under the GNU General Public License version 3 (GPLv3).
- * You may use, copy, modify, and distribute this file under the terms of GPLv3:
- *   - https://www.gnu.org/licenses/gpl-3.0.html
- *
- * THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GPLv3
- * license for more details.
- */
-
 #include <windows.h>
 #include <winreg.h>
 #include <tchar.h>
 #include <stdio.h>
-#include <winternl.h>
 #include <strsafe.h>
+#include <winternl.h>
 
-#pragma comment(lib, "advapi32.lib")
+#define SERVICE_NAME TEXT("StandbyRAMCleanerService")
+#define SERVICE_DISPLAY_NAME TEXT("Standby RAM Cleaner Service")
+
+// Define missing constants for Standby List purge
+#ifndef MemoryPurgeStandbyList
+#define MemoryPurgeStandbyList 4
+#endif
+
+#ifndef SystemMemoryListInformation
+#define SystemMemoryListInformation 0x50
+#endif
+
+#ifndef STATUS_INFO_LENGTH_MISMATCH
+#define STATUS_INFO_LENGTH_MISMATCH ((NTSTATUS)0xC0000004)
+#endif
+
+typedef LONG NTSTATUS;
+//typedef NTSTATUS(WINAPI* NtQuerySystemInformation_t)(SYSTEM_INFORMATION_CLASS, PVOID, ULONG, PULONG);
+//typedef NTSTATUS(WINAPI* NtSetSystemInformation_t)(SYSTEM_INFORMATION_CLASS, PVOID, ULONG);
+
+typedef NTSTATUS(WINAPI* NtQuerySystemInformation_t)(
+    SYSTEM_INFORMATION_CLASS SystemInformationClass,
+    PVOID SystemInformation,
+    ULONG SystemInformationLength,
+    PULONG ReturnLength
+);
+
+typedef NTSTATUS(WINAPI* NtSetSystemInformation_t)(
+    SYSTEM_INFORMATION_CLASS SystemInformationClass,
+    PVOID SystemInformation,
+    ULONG SystemInformationLength
+);
 
 #define REG_PATH TEXT("SOFTWARE\\MemoryCleaner")
 #define REG_MINFREE TEXT("MinFreeMB")
@@ -31,130 +44,108 @@
 #define DEFAULT_MINFREE_MB 2048
 #define DEFAULT_INTERVAL_SEC 10
 
-#define SERVICE_NAME TEXT("StandbyRAMCleanerService")
-#define SERVICE_DISPLAY_NAME TEXT("Standby RAM Cleaner Service")
-#define SERVICE_DESCRIPTION TEXT("Monitors memory and purges Standby List when free memory is low.")
+SERVICE_STATUS ServiceStatus;
+SERVICE_STATUS_HANDLE hStatus;
+volatile int running = 1;
 
-#define SystemPerformanceInformation 2
-#define SystemMemoryListInformation 0x50
-#define MemoryPurgeStandbyList 4
-
-typedef struct _SYSTEM_PERFORMANCE_INFORMATION {
-    LARGE_INTEGER IdleTime;
-    LARGE_INTEGER ReadTransferCount;
-    LARGE_INTEGER WriteTransferCount;
-    LARGE_INTEGER OtherTransferCount;
-    ULONG ReadOperationCount;
-    ULONG WriteOperationCount;
-    ULONG OtherOperationCount;
-    ULONG AvailablePages;
-    ULONG CommittedPages;
-    ULONG CommitLimit;
-    ULONG PeakCommitment;
-    ULONG PageFaultCount;
-    ULONG CopyOnWriteCount;
-    ULONG TransitionCount;
-    ULONG CacheTransitionCount;
-    ULONG DemandZeroCount;
-    ULONG PageReadCount;
-    ULONG PageReadIoCount;
-    ULONG CacheReadCount;
-    ULONG CacheIoCount;
-    ULONG DirtyPagesWriteCount;
-    ULONG DirtyWriteIoCount;
-    ULONG MappedPagesWriteCount;
-    ULONG MappedWriteIoCount;
-    ULONG PagedPoolPages;
-    ULONG NonPagedPoolPages;
-    ULONG PagedPoolAllocs;
-    ULONG PagedPoolFrees;
-    ULONG NonPagedPoolAllocs;
-    ULONG NonPagedPoolFrees;
-    ULONG FreePageCount;
-    ULONG ZeroPageCount;
-    ULONG ModifiedPageCount;
-    ULONG ModifiedNoWritePageCount;
-    ULONG BadPageCount;
-    ULONG PageCountByPriority[8];
-} SYSTEM_PERFORMANCE_INFORMATION;
-
-typedef NTSTATUS(NTAPI* NtQuerySystemInformation_t)(
-    ULONG SystemInformationClass,
-    PVOID SystemInformation,
-    ULONG SystemInformationLength,
-    PULONG ReturnLength
-);
-
-typedef NTSTATUS(NTAPI* NtSetSystemInformation_t)(
-    ULONG SystemInformationClass,
-    PVOID SystemInformation,
-    ULONG SystemInformationLength
-);
-
-static SERVICE_STATUS_HANDLE g_ServiceStatusHandle = NULL;
-static BOOL g_Running = TRUE;
-
-// --- Registry helper ---
-DWORD GetRegistryDWORD(LPCTSTR name, DWORD defaultValue) {
-    HKEY hKey;
-    DWORD value = defaultValue, size = sizeof(DWORD);
-    if (RegCreateKeyEx(HKEY_LOCAL_MACHINE, REG_PATH, 0, NULL,
-        REG_OPTION_NON_VOLATILE, KEY_READ | KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
-        RegQueryValueEx(hKey, name, NULL, NULL, (LPBYTE)&value, &size);
-        RegSetValueEx(hKey, name, 0, REG_DWORD, (const BYTE*)&value, sizeof(DWORD));
-        RegCloseKey(hKey);
+void LogEvent(LPCTSTR message, WORD type) {
+    HANDLE hEvent = RegisterEventSource(NULL, SERVICE_NAME);
+    if (hEvent) {
+        ReportEvent(hEvent, type, 0, 0, NULL, 1, 0, &message, NULL);
+        DeregisterEventSource(hEvent);
     }
-    return value;
 }
 
-// --- Memory info ---
-BOOL GetMemoryInfo(ULONGLONG* freeBytes, ULONGLONG* standbyBytes) {
-    HMODULE ntdll = GetModuleHandle(TEXT("ntdll.dll"));
-    NtQuerySystemInformation_t NtQuerySystemInformation =
-        (NtQuerySystemInformation_t)GetProcAddress(ntdll, "NtQuerySystemInformation");
-    if (!NtQuerySystemInformation) return FALSE;
-
-    SYSTEM_PERFORMANCE_INFORMATION info;
-    NTSTATUS status = NtQuerySystemInformation(SystemPerformanceInformation, &info, sizeof(info), NULL);
-    if (status != 0) return FALSE;
-
-    SYSTEM_INFO sysInfo;
-    GetSystemInfo(&sysInfo);
-    DWORD pageSize = sysInfo.dwPageSize;
-
-    *freeBytes = ((ULONGLONG)info.FreePageCount + (ULONGLONG)info.ZeroPageCount) * pageSize;
-
-    ULONGLONG standby = 0;
-    for (int i = 0; i < 8; i++) standby += (ULONGLONG)info.PageCountByPriority[i];
-    *standbyBytes = standby * pageSize;
-
-    return TRUE;
+DWORD GetRegistryDword(HKEY hKey, LPCTSTR valueName, DWORD defaultValue) {
+    DWORD data = defaultValue;
+    DWORD dataSize = sizeof(DWORD);
+    if (RegGetValue(hKey, NULL, valueName, RRF_RT_REG_DWORD, NULL, &data, &dataSize) != ERROR_SUCCESS) {
+        RegSetValueEx(hKey, valueName, 0, REG_DWORD, (const BYTE*)&defaultValue, sizeof(DWORD));
+        data = defaultValue;
+    }
+    return data;
 }
 
-// --- Purge standby ---
-BOOL PurgeStandbyList() {
-    HMODULE ntdll = GetModuleHandle(TEXT("ntdll.dll"));
-    NtSetSystemInformation_t NtSetSystemInformation =
-        (NtSetSystemInformation_t)GetProcAddress(ntdll, "NtSetSystemInformation");
-    if (!NtSetSystemInformation) return FALSE;
+void EnsureRegistryValues(DWORD* minFreeMB, DWORD* intervalSec) {
+    HKEY hKey;
+    if (RegCreateKeyEx(HKEY_LOCAL_MACHINE, REG_PATH, 0, NULL, 0, KEY_READ | KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+        *minFreeMB = GetRegistryDword(hKey, REG_MINFREE, DEFAULT_MINFREE_MB);
+        *intervalSec = GetRegistryDword(hKey, REG_INTERVAL, DEFAULT_INTERVAL_SEC);
+        RegCloseKey(hKey);
+    } else {
+        *minFreeMB = DEFAULT_MINFREE_MB;
+        *intervalSec = DEFAULT_INTERVAL_SEC;
+    }
+}
 
+void PurgeStandby(NtSetSystemInformation_t NtSetSystemInformation) {
     int param = MemoryPurgeStandbyList;
     NTSTATUS status = NtSetSystemInformation(SystemMemoryListInformation, &param, sizeof(param));
-    return status == 0;
+    if (status == 0) {
+        LogEvent(TEXT("Standby cache purged successfully."), EVENTLOG_INFORMATION_TYPE);
+    } else {
+        LogEvent(TEXT("Failed to purge standby cache."), EVENTLOG_ERROR_TYPE);
+    }
 }
 
-// --- Service functions ---
-void WINAPI ServiceCtrlHandler(DWORD ctrlCode) {
-    switch (ctrlCode) {
-    case SERVICE_CONTROL_STOP:
-        g_Running = FALSE;
-        if (g_ServiceStatusHandle) {
-            SERVICE_STATUS status = { 0 };
-            status.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
-            status.dwCurrentState = SERVICE_STOP_PENDING;
-            status.dwControlsAccepted = 0;
-            SetServiceStatus(g_ServiceStatusHandle, &status);
+BOOL CheckMemoryPrivileges(NtSetSystemInformation_t NtSetSystemInformation) {
+    int param = MemoryPurgeStandbyList;
+    NTSTATUS status = NtSetSystemInformation(SystemMemoryListInformation, &param, sizeof(param));
+    if (status == 0) return TRUE;
+    LogEvent(TEXT("Service does not have sufficient privileges to purge Standby List."), EVENTLOG_ERROR_TYPE);
+    return FALSE;
+}
+
+void CheckMemoryLoop(NtQuerySystemInformation_t NtQuerySystemInformation,
+                     NtSetSystemInformation_t NtSetSystemInformation) {
+    while (running) {
+        SYSTEM_PERFORMANCE_INFORMATION spi;
+
+        NTSTATUS status = NtQuerySystemInformation(SystemPerformanceInformation,
+                                                   &spi, sizeof(spi), NULL);
+        if (status == 0) { // STATUS_SUCCESS
+            DWORD pageSize = 4096; // обычно 4 KB
+            ULONGLONG freeBytes = (ULONGLONG)spi.AvailablePages * pageSize;
+            DWORD freeMB = (DWORD)(freeBytes / (1024 * 1024));
+
+            DWORD minFreeMB, intervalSec;
+            EnsureRegistryValues(&minFreeMB, &intervalSec);
+
+            // Если Available < порога, очищаем Standby
+            if (freeMB < minFreeMB) {
+                int param = 4; // MemoryPurgeStandbyList
+                NTSTATUS purgeStatus = NtSetSystemInformation(0x50 /*SystemMemoryListInformation*/,
+                                                              &param, sizeof(param));
+                TCHAR msg[256];
+                if (purgeStatus == 0) {
+                    StringCchPrintf(msg, 256, TEXT("Standby purged. Free memory: %lu MB"), freeMB);
+                    LogEvent(msg, EVENTLOG_INFORMATION_TYPE);
+                } else {
+                    StringCchPrintf(msg, 256, TEXT("Failed to purge standby. Free memory: %lu MB"), freeMB);
+                    LogEvent(msg, EVENTLOG_ERROR_TYPE);
+                }
+            } else {
+                /*TCHAR msg[256];
+                StringCchPrintf(msg, 256, TEXT("Memory OK. Free: %lu MB, Threshold: %lu MB"), freeMB, minFreeMB);
+                LogEvent(msg, EVENTLOG_INFORMATION_TYPE);*/
+            }
+
+            Sleep(intervalSec * 1000);
+        } else {
+            TCHAR msg[128];
+            StringCchPrintf(msg, 128, TEXT("NtQuerySystemInformation failed. Status=0x%X"), status);
+            LogEvent(msg, EVENTLOG_ERROR_TYPE);
+            Sleep(5000);
         }
+    }
+}
+
+void WINAPI ServiceCtrlHandler(DWORD CtrlCode) {
+    switch (CtrlCode) {
+    case SERVICE_CONTROL_STOP:
+        ServiceStatus.dwCurrentState = SERVICE_STOP_PENDING;
+        running = 0;
+        SetServiceStatus(hStatus, &ServiceStatus);
         break;
     default:
         break;
@@ -162,67 +153,109 @@ void WINAPI ServiceCtrlHandler(DWORD ctrlCode) {
 }
 
 void WINAPI ServiceMain(DWORD argc, LPTSTR* argv) {
-    g_ServiceStatusHandle = RegisterServiceCtrlHandler(SERVICE_NAME, ServiceCtrlHandler);
-    if (!g_ServiceStatusHandle) return;
+    ServiceStatus.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
+    ServiceStatus.dwControlsAccepted = SERVICE_ACCEPT_STOP;
+    ServiceStatus.dwCurrentState = SERVICE_START_PENDING;
+    ServiceStatus.dwWin32ExitCode = 0;
+    ServiceStatus.dwCheckPoint = 0;
+    ServiceStatus.dwWaitHint = 0;
 
-    SERVICE_STATUS status = { 0 };
-    status.dwServiceType = SERVICE_WIN32_OWN_PROCESS;
-    status.dwCurrentState = SERVICE_RUNNING;
-    status.dwControlsAccepted = SERVICE_ACCEPT_STOP;
-    SetServiceStatus(g_ServiceStatusHandle, &status);
+    hStatus = RegisterServiceCtrlHandler(SERVICE_NAME, ServiceCtrlHandler);
+    SetServiceStatus(hStatus, &ServiceStatus);
 
-    while (g_Running) {
-        DWORD minFreeMB = GetRegistryDWORD(REG_MINFREE, DEFAULT_MINFREE_MB);
-        DWORD intervalSec = GetRegistryDWORD(REG_INTERVAL, DEFAULT_INTERVAL_SEC);
+    LogEvent(TEXT("Standby RAM Cleaner Service started."), EVENTLOG_INFORMATION_TYPE);
 
-        ULONGLONG freeBytes, standbyBytes;
-        if (GetMemoryInfo(&freeBytes, &standbyBytes)) {
-            double freeMB = freeBytes / (1024.0 * 1024.0);
-            double standbyMB = standbyBytes / (1024.0 * 1024.0);
-            if (freeMB < minFreeMB) {
-                PurgeStandbyList();
-            }
-        }
+    ServiceStatus.dwCurrentState = SERVICE_RUNNING;
+    SetServiceStatus(hStatus, &ServiceStatus);
 
-        Sleep(intervalSec * 1000);
+    NtQuerySystemInformation_t NtQuerySystemInformation = (NtQuerySystemInformation_t)GetProcAddress(GetModuleHandle(TEXT("ntdll.dll")), "NtQuerySystemInformation");
+    NtSetSystemInformation_t NtSetSystemInformation = (NtSetSystemInformation_t)GetProcAddress(GetModuleHandle(TEXT("ntdll.dll")), "NtSetSystemInformation");
+
+    if (!NtQuerySystemInformation || !NtSetSystemInformation) {
+        LogEvent(TEXT("Failed to locate NtQuerySystemInformation/NtSetSystemInformation."), EVENTLOG_ERROR_TYPE);
+        ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+        SetServiceStatus(hStatus, &ServiceStatus);
+        return;
     }
 
-    status.dwCurrentState = SERVICE_STOPPED;
-    SetServiceStatus(g_ServiceStatusHandle, &status);
+    if (!CheckMemoryPrivileges(NtSetSystemInformation)) {
+        ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+        SetServiceStatus(hStatus, &ServiceStatus);
+        return;
+    }
+
+    CheckMemoryLoop(NtQuerySystemInformation, NtSetSystemInformation);
+
+    ServiceStatus.dwCurrentState = SERVICE_STOPPED;
+    SetServiceStatus(hStatus, &ServiceStatus);
+    LogEvent(TEXT("Standby RAM Cleaner Service stopped."), EVENTLOG_INFORMATION_TYPE);
 }
 
-// --- Main ---
-int _tmain(int argc, TCHAR* argv[]) {
+void InstallService(LPCTSTR exePath) {
+    SC_HANDLE hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CREATE_SERVICE);
+    if (!hSCManager) return;
+
+    SC_HANDLE hService = OpenService(hSCManager, SERVICE_NAME, SERVICE_QUERY_STATUS);
+    if (hService) {
+        CloseServiceHandle(hService);
+        CloseServiceHandle(hSCManager);
+        return;
+    }
+
+    hService = CreateService(
+        hSCManager,
+        SERVICE_NAME,
+        SERVICE_DISPLAY_NAME,
+        SERVICE_ALL_ACCESS,
+        SERVICE_WIN32_OWN_PROCESS,
+        SERVICE_AUTO_START,
+        SERVICE_ERROR_NORMAL,
+        exePath,
+        NULL, NULL, NULL, NULL, NULL
+    );
+
+    if (hService) {
+        StartService(hService, 0, NULL);
+        LogEvent(TEXT("Standby RAM Cleaner Service installed and started."), EVENTLOG_INFORMATION_TYPE);
+        CloseServiceHandle(hService);
+    }
+
+    CloseServiceHandle(hSCManager);
+}
+
+void UninstallService() {
+    SC_HANDLE hSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_CONNECT);
+    if (!hSCManager) return;
+
+    SC_HANDLE hService = OpenService(hSCManager, SERVICE_NAME, DELETE | SERVICE_STOP | SERVICE_QUERY_STATUS);
+    if (hService) {
+        SERVICE_STATUS ss;
+        ControlService(hService, SERVICE_CONTROL_STOP, &ss);
+        DeleteService(hService);
+        LogEvent(TEXT("Standby RAM Cleaner Service stopped and uninstalled."), EVENTLOG_INFORMATION_TYPE);
+        CloseServiceHandle(hService);
+    }
+
+    CloseServiceHandle(hSCManager);
+}
+
+int main(int argc, char* argv[]) {
+    TCHAR exePath[MAX_PATH];
+    GetModuleFileName(NULL, exePath, MAX_PATH);
+
     if (argc > 1) {
-        if (_tcscmp(argv[1], TEXT("/install")) == 0) {
-            SC_HANDLE scManager = OpenSCManager(NULL, NULL, SC_MANAGER_CREATE_SERVICE);
-            if (!scManager) return 1;
-            TCHAR path[MAX_PATH];
-            GetModuleFileName(NULL, path, MAX_PATH);
-            SC_HANDLE sc = CreateService(scManager, SERVICE_NAME, SERVICE_DISPLAY_NAME,
-                SERVICE_ALL_ACCESS, SERVICE_WIN32_OWN_PROCESS, SERVICE_AUTO_START,
-                SERVICE_ERROR_NORMAL, path, NULL, NULL, NULL, NULL, NULL);
-            if (sc) CloseServiceHandle(sc);
-            CloseServiceHandle(scManager);
+        if (_stricmp(argv[1], "/install") == 0) {
+            InstallService(exePath);
             return 0;
         }
-        else if (_tcscmp(argv[1], TEXT("/uninstall")) == 0) {
-            SC_HANDLE scManager = OpenSCManager(NULL, NULL, SC_MANAGER_CONNECT);
-            if (!scManager) return 1;
-            SC_HANDLE sc = OpenService(scManager, SERVICE_NAME, DELETE | SERVICE_STOP | SERVICE_QUERY_STATUS);
-            if (sc) {
-                SERVICE_STATUS ss;
-                ControlService(sc, SERVICE_CONTROL_STOP, &ss);
-                DeleteService(sc);
-                CloseServiceHandle(sc);
-            }
-            CloseServiceHandle(scManager);
+        if (_stricmp(argv[1], "/uninstall") == 0) {
+            UninstallService();
             return 0;
         }
     }
 
     SERVICE_TABLE_ENTRY ServiceTable[] = {
-        { (LPTSTR)SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain },
+        { SERVICE_NAME, (LPSERVICE_MAIN_FUNCTION)ServiceMain },
         { NULL, NULL }
     };
     StartServiceCtrlDispatcher(ServiceTable);
